b66a37b28c311aa31de6e4eb27cf64a92f0ac426
katskurka <kati.skurka@gmail.com>
2021-08-09 21:03:14 -0400

    added get by team id route to controllers folder

--- controllers/teams.js
+++ controllers/teams.js
@@ -4,4 +4,11 @@ const listOfTeams = (req, res) => {
   return res.send(teams)
 }
 
-module.exports = listOfTeams
+const getByTeamId = (req, res) => {
+  const { id } = req.params
+  const specificTeam = teams.filter(team => team.id === parseInt(id))
+
+  return res.send(specificTeam)
+}
+
+module.exports = { listOfTeams, getByTeamId }
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
-const listOfTeams = require('./controllers/teams')
+const { listOfTeams, getByTeamId } = require('./controllers/teams')
 
-// require
-const teams = require('./teams')
-
-// home
+// home route
 app.get('/teams', listOfTeams)
 
+// team specific route
+app.get('/teams/:id', getByTeamId)
+
+/* eslint-disable no-console */
 // setup port
 app.listen(3000, () => {
   console.log('Server is up!')
